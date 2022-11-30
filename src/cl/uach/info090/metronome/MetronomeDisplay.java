1229642a65c23951452d4fc0de0db5de6dc9dac9
maxilamano <maxilamano@gmail.com>
2022-11-30 18:42:55 -0300

    MetronomeGUI  and MetronomeDisplay

---
+++ src/cl/uach/info090/metronome/MetronomeDisplay.java
@@ -0,0 +1,5 @@
+package cl.uach.info090.metronome;
+
+public interface MetronomeDisplay {
+	public void tick();
+}
--- src/cl/uach/info090/metronome/MetronomeGUI.java
+++ src/cl/uach/info090/metronome/MetronomeGUI.java
@@ -1,11 +1,22 @@
 package cl.uach.info090.metronome;
 
 public class MetronomeGUI {
-	private MetronomeGUI metronome = new MetronomeGUI();
+	private static MetronomeGUI metronome = new MetronomeGUI();
 
 	private MetronomeGUI() {
 		super();
 	}
 	
+	public static MetronomeGUI getInstance() {
+		if (metronome == null) {
+			metronome = new MetronomeGUI();
+		}
+		return metronome;
+	}
+	//---------------------------main---------------------------//
+	public static void main(String[] args) {
+		MetronomeGUI metronome = MetronomeGUI.getInstance();
+		
+	}
 	
 }
