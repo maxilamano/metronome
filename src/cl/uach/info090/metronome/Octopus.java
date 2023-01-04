999278793371b03b8a43b9e8f3f64019e948a13b
maxilamano <maxilamano@gmail.com>
2023-01-04 17:47:31 -0300

    octopus indicator

---
+++ src/cl/uach/info090/metronome/Octopus.java
@@ -0,0 +1,15 @@
+package cl.uach.info090.metronome;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+public class Octopus extends JLabel{
+
+}
